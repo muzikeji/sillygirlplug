2125a34517b32871988b8082ae5a88123fb18612
木子李 <52457863+muzikeji@users.noreply.github.com>
2022-06-17 19:37:33 +0800

    Add files via upload

---
+++ 发邮件.js
@@ -0,0 +1,32 @@
+// [rule: raw \[CQ:at,qq=(\d+),text=([^\[]+)]\s*邮件([\s\S]*)]
+// [rule: raw 发邮件([\s\S]*)]
+//[priority: 113]优先级
+// [admin: true]
+
+function main() {
+    var url = get("murl")
+    var user = GetUserID()
+    var chatID = GetChatID()
+    var username = GetUsername()
+    var userID = param(1) + "@qq.com"
+    var title = param(3)
+    if (ImType() != "qq" || !title  ) {
+    var title = param(1)
+        sendText("请输入收件人邮箱：")
+    var userID = input(180000)
+    } else {}
+sendText("请在3分钟输入邮件内容:")
+    var content = input(180000)
+    var contents = content.replace(/【/g, "<h2>").replace(/】/g, "</h2>").replace(/〖/g, "<h3>").replace(/〗/g, "</h3>").replace(/／/g, "<br>").replace(/〔/g, "<b>").replace(/〕/g, "</b>")
+    var cont = encodeURI(contents)
+    var database = request({ // 内置http请求函数
+        url: "http://" + url + "/api/email/api.php?address="+ userID + "&name=" + title + "&certno=" + cont,
+        //请求链接
+        "method": "get",
+        //请求方法
+        "dataType": "json",
+        //指定json类型数据
+    })
+sendText((database.msg))
+}
+main() 
