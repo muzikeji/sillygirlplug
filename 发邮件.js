aa91fe91a7910bc0e88826a5f5f9fad31292a5e1
muzikeji <56794501@qq.com>
2022-12-17 09:00:51 +0800

    更新发邮件脚本

--- 发邮件.js
+++ 发邮件.js
@@ -12,12 +12,12 @@ function main() {
     var title = param(3)
     if (ImType() != "qq" || !title  ) {
     var title = param(1)
-        sendText("请输入收件人邮箱：")
+        sendText("请输入收件人邮箱：\n【H2】〖H3〗『H4』「H5」〔加粗〕br 换行")
     var userID = input(180000)
     } else {}
 sendText("请在3分钟输入邮件内容:")
     var content = input(180000)
-    var contents = content.replace(/【/g, "<h2>").replace(/】/g, "</h2>").replace(/〖/g, "<h3>").replace(/〗/g, "</h3>").replace(/——/g, "<br>").replace(/〔/g, "<b>").replace(/〕/g, "</b>")
+    var contents = content.replace(/【/g, "<h2>").replace(/】/g, "</h2>").replace(/〖/g, "<h3>").replace(/〗/g, "</h3>").replace(/『/g, "<h4>").replace(/』/g, "</h4>").replace(/「/g, "<h5>").replace(/」/g, "</h5>").replace(/br/g, "<br>").replace(/〔/g, "<b>").replace(/〕/g, "</b>")
     var cont = encodeURI(contents)
     var database = request({ // 内置http请求函数
         url: "http://" + url + "/api/email/api.php?address="+ userID + "&name=" + title + "&certno=" + cont,
@@ -29,4 +29,4 @@ sendText("请在3分钟输入邮件内容:")
     })
 sendText((database.msg))
 }
-main() 
+main()
