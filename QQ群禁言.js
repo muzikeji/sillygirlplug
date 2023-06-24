d2239d0dc3b5a7aa07148ed800381dcbef1ccf02
muzikeji <56794501@qq.com>
2023-06-24 10:09:28 +0800

    修改部分正则表达式

--- QQ群禁言.js
+++ QQ群禁言.js
@@ -1,5 +1,5 @@
-// [rule: raw \[CQ:at,qq=(\d+),text=([^\[]+)]\s*/ban\s+(\d+)]
-// [rule: raw \[CQ:at,qq=(\d+),text=([^\[]+)]\s*禁言\s+(\d+)]
+// [rule: raw \[CQ:at,qq=(\d+)]\s*/ban\s+(\d+)]
+// [rule: raw \[CQ:at,qq=(\d+)]\s*禁言\s+(\d+)]
 // [admin: true]
 GroupBan(param(1), +param(3))
 sendText("[CQ:at,qq=" + param(1) + ",text=" + param(2) + "] 已被禁言"+param(3)+"秒。")
