146134cb6f5f7b2fdf5816dbe5ecfe4ccf6db539
56794501@qq.com <56794501@qq.com>
2023-12-03 13:11:08 +0800

    update 小爱帮你答.js

--- Oldverplug-in/小爱帮你答.js
+++ Oldverplug-in/小爱帮你答.js
@@ -1,8 +1,9 @@
 // [rule: ?？]其他关键字
-// [rule: ?啊]其他关键字
+// [rule: ?吗]其他关键字
 // [rule: 小爱?]其他关键字
 // [rule: raw \[CQ:at,qq=1975537275,text=([^\[]+) ([\s\S]*)]
 // [rule: raw \[CQ:at,qq=304625534,text=([^\[]+) ([\s\S]*)]
+// [rule: raw \[CQ:at,qq=731428866,text=([^\[]+) ([\s\S]*)]
 //[disable: false]是否禁用
 //[imType:qq] 白名单
 function main() {
@@ -10,27 +11,34 @@ function main() {
     var input1 = param(1)
     var input2 = param(2)
     if (param(2) != "") {
-        var data = request({
-            url: "http://www.tuling123.com/openapi/api?key=77f2530f4d83405bb3a01a30014f6117&info=" + param(2),
-            //请求链接
-            "method": "get",
-            //请求方法
-            "dataType": "json",
-            //这里接口直接返回文本，所以不需要指定json类型数据
-        });
-
-        sendText(data.text)
-    } else {
-        var data = request({
-            url: "http://www.tuling123.com/openapi/api?key=77f2530f4d83405bb3a01a30014f6117&info=" + param(1),
-            //请求链接
-            "method": "get",
-            //请求方法
-            "dataType": "json",
-            //这里接口直接返回文本，所以不需要指定json类型数据
-        });
-
-        sendText(data.text)
+var text = input2
+   } else {var text = input2
+}
+  var data = request(
+    {
+      url: "http://openapi.turingapi.com/openapi/api/v2",
+      method: "post",
+      headers: {
+        "Content-Type": 'application/json',
+      },
+body: JSON.stringify({
+    reqType: 0,
+    perception: {
+      inputText: {
+        text: text
+      }
+    },
+    userInfo: {
+      apiKey: "",//图灵apikey
+      userId: "460134"
     }
+  })
+
+}  );
+
+ 
+
+var data = JSON.parse(data)
+              sendText(data.results[0].values.text)
 }
 main()
